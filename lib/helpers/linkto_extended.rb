9317ddfc4333cbd24ef45c752f6f2a5b453a2afc
Fabio Scaccabarozzi <fsvm88@gmail.com>
2013-10-19 21:45:59 +0200

    Fix linkto helper to do the right thing.

--- lib/helpers/linkto_extended.rb
+++ lib/helpers/linkto_extended.rb
@@ -1,12 +1,13 @@
 module LinkToExtended
 
-	def link_to_unless_current_withid(text, item, extra_attrs={})
-		if extra_attrs.empty?
-			params = { :id => 'currently_active_item' }
-		else
-			params = extra_attrs.merge({ :id => 'currently_active_item' })
+	def link_to_unless_current_withid(text, target, extra_attrs={})
+		path = target.is_a?(String) ? target : target.path
+
+		if @item_rep && @item_rep.path == path
+			return "<span class=\"active\" id=\"currently_active_item\" title=\"You're here!\">#{text}</span>"
 		end
-		link_to_unless_current(text, item, params)
+
+		link_to_unless_current(text, target, extra_attrs)
 	end
 
 end
