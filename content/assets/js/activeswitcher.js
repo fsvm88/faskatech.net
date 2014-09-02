f6613d04f3f3cb74d5d648f043f97f7fcf086b5c
Fabio Scaccabarozzi <fsvm88@gmail.com>
2014-09-02 12:54:08 +0200

    Make navbar JS easier to read and more portable (works also on the brand-new "Coding" menu).

--- content/assets/js/activeswitcher.js
+++ content/assets/js/activeswitcher.js
@@ -1,7 +1,7 @@
 $(document).ready(
 	function() {
 		var curItem = $('.active');
-		var toCheck = curItem.parent().parent().parent();
+		var toCheck = curItem.parents('li.dropdown');
 
 		if (toCheck.children('.dropdown-toggle')) {
 	 		toCheck.children('.dropdown-toggle').addClass('active');
